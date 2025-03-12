2bcb817c26a4947f88f87a407ed5074b0cac3e38
JonasPalms <jonas.p.sorensen@gmail.com>
2025-03-12 21:29:30 +0100

    add site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@
 import { defineConfig } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
 
-import icon from 'astro-icon';
+import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-
+  site: 'https://jonaspalms.github.io/my-portfolio',
   integrations: [icon()],
 })
